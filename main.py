5cf7cada535207b5848b8b1acc9e90f0422969f4
shaunjohnson79 <120249330+shaunjohnson79@users.noreply.github.com>
2022-12-27 14:39:30 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,111 @@
+from readXMLData import parseXML
+import pandas as pd
+import time
+
+
 def main():
     print("Program Start")
+    #define the filenames
+    fileInfo={}
+    fileInfo.update({'XML':'settingsQuick.xml'})
+    fileInfo.update({'XLS_source':'20210323 Hinterkipper_de en_finala.xlsx'})
+    fileInfo.update({'XLS_master':'20210323 Hinterkipper_de en_final_master.xlsm'})
+
+    # Read in the settings
+    settings = parseXML(fileInfo['XML'])
+    #print(settings)
     
+    # Read the source XLS
+    XLSSource={}
+    for sheet in settings.getSheetNames():
+        #read the sheet
+        fileToRead=fileInfo['XLS_source']
+        sheetToRead=sheet
+        headerRow=settings.getNameRow(sheet)-1
+        tempSheet=pd.read_excel(fileToRead, sheet_name=sheetToRead, header=headerRow)
+        
+        #Add the search column
+        partNumberList=settings.getItemListFromName(sheet,'partNumber') 
+        tempSheet['search'] = tempSheet[partNumberList].agg('-'.join, axis=1)
+
+        #Add to dictionary
+        XLSSource.update({sheet:tempSheet})
+        print("Read {} : {}".format(fileToRead,sheetToRead))
+        
+    # Read the master XLS
+    XLSMaster={}
+    for sheet in settings.getSheetNames():
+        #read the sheet
+        fileToRead=fileInfo['XLS_master']
+        sheetToRead=sheet
+        headerRow=settings.getNameRow(sheet)-1
+        tempSheet=pd.read_excel(fileToRead, sheet_name=sheetToRead, header=headerRow)
+        
+        #Add the search column
+        partNumberList=settings.getItemListFromName(sheet,'partNumber') 
+        tempSheet['search'] = tempSheet[partNumberList].agg('-'.join, axis=1)
+
+        #Add to dictionary
+        XLSMaster.update({sheet:tempSheet})
+        print("Read {} : {}".format(fileToRead,sheetToRead))
+
+    # Look for code pairs
+    for sheet in settings.getSheetNames():
+        
+        
+        # get the data
+        DFSource=XLSSource[sheet]
+        DFSource=DFSource.set_index('search')
+        DFSource['status']='current'
+        DFMaster=XLSMaster[sheet]
+        DFMaster=DFMaster.set_index('search')
+        print(type(DFMaster))
+        print(DFMaster.info)
+        print(DFSource.info)
+        
+        #get the columns to compare
+        compareList=settings.getItemListFromName(sheet,'compare') 
+        for compareColumn in compareList:
+            if compareColumn not in DFSource.columns or compareColumn not in DFMaster.columns:
+                compareList.remove(compareColumn)
+                print("Remove the following coloum from compare list\n\t{}".format(compareColumn))
+                time.sleep(5)
+        
+        
+        #for SP, source in DFSource.iterrows():
+        print(DFSource)
+        for SP, source in DFSource.iterrows():
+            # Get source and master
+            if SP not in DFMaster.index:
+                DFSource.at[SP,'status']='new'
+                continue
+            
+            for compareColumn in compareList:
+                tempSource=str(DFSource.at[SP,compareColumn])
+                tempMaster=str(DFMaster.at[SP,compareColumn])
+                if tempSource != tempMaster :
+                    DFSource.at[SP,'status']='change'
+                    
+                
+                
+                
+            #check the rows for 
+            #print(master.info)
+            quit()
+            
+            
+            
+            
+            
+
+                
+            
+            #sourceRow = source
+            #print(sourceRow)
+
+        
+        
+        
     
 
 if __name__ == "__main__":
--- readXMLData.py
+++ readXMLData.py
@@ -1,30 +1,79 @@
-import xml.etree.ElementTree as ET
+from lxml import etree
 
 class parseXML(object):    
     def __init__(self,xmlFileName):
-        self.doc=ET.parse(xmlFileName)
+        
+        self.doc=etree.parse(xmlFileName)
         self.root = self.doc.getroot()
-
+        
+    def __repr__(self):   
+        etree.indent(self.doc, space="  ")
+        returnString=etree.tostring(self.doc, pretty_print=True).decode()
+        return returnString
+    
+    def __getSheetBranch(self,tree,sheetName) -> object:
+        searchString=".sheet/[name='{}']".format(sheetName)
+        branch=tree.findall(searchString)
+        if(len(branch)==1):
+            branch=branch[0]
+        else:
+            print('not able to find value')
+            return
+        return branch
+    
+    def __getColumnsBranch(self,tree,sheetName) -> object:
+        branch = self.__getSheetBranch(tree,sheetName)
+        branch=branch.find('columns')
+        return branch
+    
+    def __getItemList(self,tree) -> list:
+        itemList=[]
+        for tempItem in tree.iter('item'):
+            itemList.append(tempItem.text)
+        return itemList
+    
     def getSheetNames(self) -> list:
         sheetNames=[]
         for sheet in self.root.findall('sheet'):  
-            name=sheet.find('name')
-            sheetNames.append(name.text)
+            name=sheet.find('name').text
+            sheetNames.append(name)
         return sheetNames
     
-    def         
+    def getNameRow(self,sheetName) -> int:
+        rowNumber=None
+        tempSheet = self.__getSheetBranch(self.root,sheetName)
+        rowNumber = int(tempSheet.find('nameRow').text)
+        return rowNumber
     
+    def getPartNumberString(self,sheetName) -> list:
+        tempColumn=self.__getColumnsBranch(self.root,sheetName)
+        partNumber=tempColumn.find('partNumber')
+        partNumberString=self.__getItemList(partNumber)
+        return partNumberString
     
-
+    def getItemListFromName(self,sheetName,itemName) -> list:
+        tempColumn=self.__getColumnsBranch(self.root,sheetName)
+        partNumber=tempColumn.find(itemName)
+        partNumberString=self.__getItemList(partNumber)
+        return partNumberString  
 
 def main():
     fileName='settings.xml'
     settings = parseXML("settings.xml")
     sheetNames=settings.getSheetNames()
     print(sheetNames)
-
-
-
+    for tempSheetName in sheetNames:
+        print(tempSheetName)
+        rowNumber=settings.getNameRow(tempSheetName)
+        print("rowNumber={}".format(rowNumber))
+        #partNumberString=settings.getPartNumberString(tempSheetName)
+        #print(partNumberString)
+        partNumberList=settings.getItemListFromName(tempSheetName,'partNumber')
+        print(partNumberList)
+        
+        print(settings)
+        
+        return
 
 if __name__ == '__main__':
     main()
