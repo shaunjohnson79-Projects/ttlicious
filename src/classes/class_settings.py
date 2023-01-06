dddb5964ea09ea34b117bfe7d173183e6f8e4d67
shaunjohnson79 <120249330+shaunjohnson79@users.noreply.github.com>
2023-01-06 20:49:59 +0100

    update

--- src/classes/class_settings.py
+++
@@ -1,9 +0,0 @@
-class ClassSettings():
-    def __init__(self) -> None:
-
-        self.columnNamesDictionary = {
-            cfg.statusLabels.date: cfg.statusLabels.date,
-            cfg.statusLabels.status: cfg.statusLabels.status,
-            "search": cfg.statusLabels.search,
-        }
-        return
